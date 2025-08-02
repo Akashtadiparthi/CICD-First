695e96e207cd3e8adbcceda00193dd318e789026
Akashtadiparthi <akashtadiparthi@gmail.com>
2025-08-02 16:58:17 +0530

    version 3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="app">
-      <h1>Welcome to the CICD Class</h1>
+      <h1>HELLO WORLD</h1>
       <p>This is section S211.</p>
     </div>
   )
