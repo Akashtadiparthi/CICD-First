26419729a67b16f3dd930967a882be02ce9d0f58
Akashtadiparthi <akashtadiparthi@gmail.com>
2025-08-02 17:08:21 +0530

    version-3

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className="app">
-      <h1>HELLO WORLD</h1>
+      <h1>HELLO WORLD!</h1>
       <p>This is section S211.</p>
     </div>
   )
